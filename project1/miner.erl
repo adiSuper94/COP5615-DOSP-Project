e360552c322fe4a9ad3e8f9d059409d5d3a84215
Aditya <aditarun2008@gmail.com>
2022-09-17 17:14:27 -0400

    worker: find hash with n 0s

--- project1/miner.erl
+++ project1/miner.erl
@@ -2,6 +2,19 @@
 -import(crypto, [hash/2]).
 -import(binary, [decode_unsigned/1]).
 -import(io_lib, [format/2]).
--export([test/1]).
+-export([findCoin/2]).
 
-test(Str) -> format("~64.16.0b", [decode_unsigned(hash(sha256, Str))]).
+hash(Str) -> format("~64.16.0b", [decode_unsigned(hash(sha256, Str))]).
+
+findCoin(Str, ZeroCount) -> recFindCoin(Str, ZeroCount, 0).
+
+recFindCoin(Str, ZeroCount, Count) ->
+  Prefix = string:concat("aditya.subramani;", Str),
+  Coin = string:concat(Prefix, erlang:integer_to_list(Count, 36)),
+  CoinHash = hash(Coin),
+  Result = string:substr(CoinHash, 1, ZeroCount) == lists:flatten(lists:duplicate(ZeroCount, "0")),
+  if 
+    Result == true -> io:fwrite(string:concat(string:concat(Coin, " : "), CoinHash));
+    Result == false -> recFindCoin(Str, ZeroCount, Count + 1)
+  end.
+
